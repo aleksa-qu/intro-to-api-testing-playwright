d0e8687532421130ad88b960e466d8f49924706f
37255 <aleksandra.kulakova.ndu@gmail.com>
2025-06-14 16:46:42 +0300

    Created check-list calculate risk Created DTO order Added new tests Playwright POST Correction added!

---
+++ tests/dto/order-dto.ts
@@ -0,0 +1,34 @@
+export class OrderDto {
+  status: string
+  courierId: number
+  customerName: string
+  customerPhone: string
+  comment: string
+  id: number
+
+  private constructor(
+    customerName: string,
+    customerPhone: string,
+    comment: string,
+    id: number,
+    status: string,
+    courierId: number,
+  ) {
+    this.status = status
+    this.courierId = courierId
+    this.customerName = customerName
+    this.customerPhone = customerPhone
+    this.comment = comment
+    this.id = id
+  }
+  static createOrderWithRandomData(): OrderDto {
+    return new OrderDto(
+      'John Smith',
+      '+1234567',
+      'Test comment',
+      Math.floor(Math.random() * 100),
+      'OPEN',
+      Math.floor(Math.random() * 100),
+    )
+  }
+}
--- tests/simple-solution-api.test.ts
+++ tests/simple-solution-api.test.ts
@@ -1,7 +1,7 @@
 //extra
-import axios from 'axios'
+import axios from 'axios';
 import { StatusCodes } from 'http-status-codes';
-import { expect, test } from '@playwright/test'
+import { expect, test } from '@playwright/test';
 //PUT
 test('change an order with correct id 1 should receive code 200', async () => {
   const requestBody = {
@@ -14,13 +14,13 @@ test('change an order with correct id 1 should receive code 200', async () => {
   };
 
   const requestHeaders = {
-    'api_key': '1234567890123456',
+    api_key: '1234567890123456',
   };
 
   const response = await axios.put(
     'https://backend.tallinn-learning.ee/test-orders/1',
     requestBody,
-    { headers: requestHeaders }
+    { headers: requestHeaders },
   );
 
   console.log('response status:', response.status);
@@ -30,16 +30,18 @@ test('change an order with correct id 1 should receive code 200', async () => {
 });
 //DELETE
 test('delete order with correct id 1 should receive code 204', async () => {
-  const requestHeaders: { api_key: string } = { 'api_key': '1234567890123456' };
+  const requestHeaders: { api_key: string } = { api_key: '1234567890123456' };
   const response = await axios.delete('https://backend.tallinn-learning.ee/test-orders/1', {
     headers: requestHeaders,
-  })
-  expect(response.status).toBe(StatusCodes.NO_CONTENT)
-})
+  });
+  expect(response.status).toBe(StatusCodes.NO_CONTENT);
+});
 //GET
 test('get authentication of a user with username and password as string should receive code 200', async () => {
-  const response = await axios.get('https://backend.tallinn-learning.ee/test-orders?username=test&password=test')
-  console.log('response body:', await response.data)
-  console.log('response headers:', response.headers)
-  expect(response.status).toBe(StatusCodes.OK)
-})
+  const response = await axios.get(
+    'https://backend.tallinn-learning.ee/test-orders?username=test&password=test',
+  );
+  console.log('response body:', await response.data);
+  console.log('response headers:', response.headers);
+  expect(response.status).toBe(StatusCodes.OK);
+});
